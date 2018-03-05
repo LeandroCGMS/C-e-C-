84699bcb9202bb964f226652b6ba82dbbf20a50d
LeandroCGMS <36311677+LeandroCGMS@users.noreply.github.com>
2018-03-04 20:34:25 -0400

    Add files via upload

---
+++ aula23.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+void soma();
+void soma(int n1,int n2);
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    soma();
+    soma(50,25);
+    return 0;
+}
+
+void soma(int n1,int n2){
+    int re;
+    re = n1+n2;
+    cout<<"\nSoma de "<<n1<<" com "<<n2<<" = "<<re;
+
+}
+
+void soma(){
+    int n1,n2,re;
+    n1=10;
+    n2=20;
+    re=n1+n2;
+    cout<<"\nSoma de "<<n1<<" com "<<n2<<" = "<<re;
+}
