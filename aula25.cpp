6b57f2e8554f0567ed941041639d95967f713fb8
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-03-07 10:00:42 -0400

    Add files via upload

---
+++ aula25.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+void contador(int num, int cont=0);
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    contador(20,0);
+    return 0;
+}
+
+void contador(int num, int cont){
+
+    cout << cont << "\n";
+    if(num > cont){
+        contador(num,++cont);
+    }
+
+}
---
+++ fatorial_recursivo.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+int cont = 1;
+int fatorial(int termo);
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    cout << fatorial(5);
+    return 0;
+}
+
+int fatorial(int termo){
+    if(cont == 1 && termo == 0){
+        cout << "0! = ";
+        return 1;
+    }
+
+    if(termo == 1 && cont == 1){
+        cout << " 1! = ";
+        return 1;
+    }
+
+    if(cont == 1){
+        cout << termo << "! = ";
+    }
+
+    if(termo > 1){
+        cout << termo << " x ";
+    }
+
+    if(termo == 0){
+            cout << "1 = ";
+        return 1;
+    }
+
+    cont++;
+    return termo * fatorial(termo - 1);
+
+}
