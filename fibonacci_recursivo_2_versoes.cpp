eb34ed635ea35befc30ea935af11f921c603d56d
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-03-08 09:57:31 -0400

    Update fibonacci_recursivo_2_versoes.cpp

--- fibonacci_recursivo_2_versoes.cpp
+++ fibonacci_recursivo_2_versoes.cpp
@@ -16,24 +16,25 @@ int main(){
     int op = 0;
     while(!menuValido){
             system("cls");
-        cout << "Quer usar a função recursiva da sequência de Fibonacci conhecida ou a desenvolvida por mim? [1-Conhecida, 2-Minha ou 3-Sair]\n\n\t\t\t";
+        cout << "Quer usar a funÃ§Ã£o recursiva da sequÃªncia de Fibonacci conhecida ou a desenvolvida por mim? [1-Conhecida, 2-Minha ou 3-Sair]\n\n\t\t\t";
         cin >> op;
         if(op > 0 && op < 4){
             menuValido = true;
         } else {
-            cout << "Opção inválida. Tente novamente.";
+            cout << "OpÃ§Ã£o invÃ¡lida. Tente novamente.\n\n\n\n\t\t\t\t";
+            system("pause");
         }
     }
     if(op == 2){
-        cout << "\t\t\Gerador de Sequência Fibonacci por Leandro Santos de Carvalho\n\n\t\tDigite o termo final da sequência a ser visualizada.\n\t\t\t\t\t";
+        cout << "\t\t\Gerador de SequÃªncia Fibonacci por Leandro Santos de Carvalho\n\n\t\tDigite o termo final da sequÃªncia a ser visualizada.\n\t\t\t\t\t";
         cin >> termo;
         fib(termo);
         cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
         system("pause");
         main();
     } else if(op == 1){
-        cout << "\t\t\tAlgoritmo conhecido da Sequência de Fibonacci";
-        cout << "\n\n\t\t\tDigite o termo que quer visualizar da Sequência de Fibonacci. \n\n\t\t\t";
+        cout << "\t\t\tAlgoritmo conhecido da SequÃªncia de Fibonacci";
+        cout << "\n\n\t\t\tDigite o termo que quer visualizar da SequÃªncia de Fibonacci. \n\n\t\t\t";
         cin >> termo;
         cout << "\t\t\t" << fibRPadrao(termo) << "\t\t\t\n\n\n\n";
         system("pause");
@@ -50,19 +51,19 @@ void fib(int termo){
     int trocaTermo1 = 0;
     int nTermo = termo; // SEMPRE 3
 	if(termo<=0){
-		cout << "Não pode haver ordem zero ou negativa.";
-		 //COLOCAR FUNÇÃO DE RETORNO A TELA PRINCIPAL
+		cout << "NÃ£o pode haver ordem zero ou negativa.";
+		 //COLOCAR FUNÃ‡ÃƒO DE RETORNO A TELA PRINCIPAL
 	}
     if(termo < 2){
-        cout << termo<<"º elemento de Fibonacci: "<< "0. ou ...";
+        cout << termo<<"Âº elemento de Fibonacci: "<< "0. ou ...";
     }
 
     if (termo == 2){
-    	cout << "Sequência de Fibonacci até o "<<termo<<"º elemento: "<<"0, 1. ou ...";
+    	cout << "SequÃªncia de Fibonacci atÃ© o "<<termo<<"Âº elemento: "<<"0, 1. ou ...";
 	}
 
     if(termo > 2 && cont == 3){
-        cout << "Sequência de Fibonacci até o " << termo << "º elemento: " << Termo1 << ", " << Termo2 << ", ";
+        cout << "SequÃªncia de Fibonacci atÃ© o " << termo << "Âº elemento: " << Termo1 << ", " << Termo2 << ", ";
         Termo2 += Termo1;                                                       //0              //1
          cout << Termo2 << ", ";
          Termo1 =  Termo2;
