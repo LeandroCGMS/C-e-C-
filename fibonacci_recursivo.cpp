f6f8c062bbc462bbd5d75a9c86be4e6d6f7d83fb
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-03-07 20:16:30 -0400

    Update fibonacci_recursivo.cpp

--- fibonacci_recursivo.cpp
+++ fibonacci_recursivo.cpp
@@ -9,8 +9,12 @@ int Termo2 = 1;
 int cont = 3;
 void fib(int termo);
 int main(){
+    int termo;
     setlocale(LC_ALL,"Portuguese");
-    fib(6);
+    cout << "\t\tFalta apenas a opÃ§Ã£o de reexecutar.\n\t\t\Gerador de SequÃªncia Fibonacci\n\n\t\tDigite o termo final da sequÃªncia a ser visualizada.\n\t\t\t\t\t";
+    cin >> termo;
+    fib(termo);
+    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
     return 0;
 }
 
@@ -18,19 +22,19 @@ void fib(int termo){
     int trocaTermo2 = 0;
     int nTermo = termo; // SEMPRE 3
 	if(termo<=0){
-		cout << "Não pode haver ordem zero ou negativa.";
-		 //COLOCAR FUNÇÃO DE RETORNO A TELA PRINCIPAL
+		cout << "NÃ£o pode haver ordem zero ou negativa.";
+		 //COLOCAR FUNÃ‡ÃƒO DE RETORNO A TELA PRINCIPAL
 	}
     if(termo < 2){
-        cout << termo<<"º elemento de Fibonacci: "<< "0.";
+        cout << termo<<"Âº elemento de Fibonacci: "<< "0.";
     }
 
     if (termo == 2){
-    	cout << "Sequência de Fibonacci até o "<<termo<<"º elemento: "<<"0, 1.";
+    	cout << "SequÃªncia de Fibonacci atÃ© o "<<termo<<"Âº elemento: "<<"0, 1.";
 	}
     //return fib(termo-1) + fib(termo-2);
     if(termo > 2 && cont == 3){
-        cout << "Sequência de Fibonacci até o " << termo << "º elemento: " << Termo1 << ", " << Termo2 << ", ";
+        cout << "SequÃªncia de Fibonacci atÃ© o " << termo << "Âº elemento: " << Termo1 << ", " << Termo2 << ", ";
         Termo1 = Termo2; // 0 PASSOU A SER 1
         Termo2 += Termo1; // 1 PASSOU A SER 2
          cout << Termo2 << ", "; // 0, 1, 2
