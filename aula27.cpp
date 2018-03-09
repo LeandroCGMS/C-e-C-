51d702ab26fb9b0e7b32d5a6890d1f79f43e673b
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-03-08 20:50:59 -0400

    Add files via upload

---
+++ aula27.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+#include <windows.h>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    enum armas{fuzil=100, revolver=8, rifle=12, escopeta=1};
+
+    armas armaSel;
+
+    armaSel = fuzil;
+
+    cout << rifle;
+    return 0;
+}
