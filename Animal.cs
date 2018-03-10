fb54c883911b94e279ed6cc2dc299e5513c844d1
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-03-09 21:53:05 -0400

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificadores_de_Acesso
+{
+    class Animal
+    {
+        private int idade = 10;
+        public double peso = 50.4;
+
+        public int ObterIdade()
+        {
+            return this.idade;
+        }
+
+        public double ObterPeso
+        {
+            get { return this.peso; }
+        }
+    }
+}
---
+++ Modificadores_de_Acesso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificadores_de_Acesso
+{
+    class Modificadores_de_Acesso
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            double peso;
+
+            Animal bicho = new Animal();
+            idade = bicho.ObterIdade();
+            peso = bicho.ObterPeso;
+
+            Console.WriteLine("A idade é : {0}", idade);
+            Console.WriteLine("O peso é: {0:F}", peso);
+            Console.ReadLine();
+        }
+    }
+}
