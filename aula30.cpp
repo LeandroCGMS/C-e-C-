03a39390eca545a6246370a016460698899712e9
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-03-12 21:16:14 -0400

    Add files via upload

---
+++ aula30.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+#include <queue>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    queue <string> cartas;
+    if(cartas.empty()){
+            cout << "Com método empty()\nAté, aqui, fila está vazia.\n\n";
+        } else {
+            cout << "Com método empty()\nfila com cartas.\n\n";
+        }
+
+        if(cartas.size() == 0){
+            cout << "Com método size()\nAté, aqui, fila está vazia.\n\n";
+        } else {
+            cout << "Com método size()\nfila com cartas.\n\n";
+        }
+
+        cartas.push("Rei de Copas");
+        cartas.push("Rei de Espadas");
+        cartas.push("Rei de Ouros");
+        cartas.push("Rei de Paus");
+
+        cout << "Tamanho da fila: " << cartas.size() << "\n";
+        cout << "Primeira Carta: " << cartas.front() << "\n";
+        cout << "Última Carta: " << cartas.back() << "\n";
+
+        while(!cartas.empty()){
+            cout << "Primeira Carta: " << cartas.front() << "\n";
+            cartas.pop();
+        }
+
+    return 0;
+}
