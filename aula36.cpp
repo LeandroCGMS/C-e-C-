aab5ae9d53cf672bba43b57494ff57d1c44e2a52
Leandro Santos de Carvalho <36311677+LeandroCGMS@users.noreply.github.com>
2018-03-15 20:36:57 -0400

    Add files via upload

---
+++ aula36.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+
+    string veiculo="Carro";
+    string *pv = & veiculo;
+
+    cout << pv << "\n" << &veiculo;
+
+    *pv="Moto";
+
+    cout << "\n" << veiculo << "\n" << *pv;
+
+    return 0;
+}
