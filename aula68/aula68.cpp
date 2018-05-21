9e22adcd4baf3b48e84815a5cd667e9c72e7e382
LeandroCGMS <leandrocgms1986@outlook.com>
2018-05-21 12:15:33 -0400

    Aula 68
    Modificador Protected.

---
+++ aula68/aula68.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <locale.h>
+#include <stdlib.h>
+
+using namespace std;
+
+class Veiculo {
+private:
+    int velMax;
+    int potencia;
+public:
+    int rodas;
+    const char* nome;
+protected:
+    int portas;
+    const char* cor;
+};
+
+class Carro:public Veiculo {
+public:
+    Carro() {
+        //velMax = 180;
+        //potencia = 150;
+        rodas = 4;
+        nome = "Carro";
+        portas = 4;
+        cor = "Vermelho";
+
+        //cout << velMax << endl;
+        //cout << potencia << endl;
+        cout << rodas << endl;
+        cout << nome << endl;
+        cout << portas << endl;
+        cout << cor << endl;
+    }
+};
+
+class Moto {
+public:
+    Carro c;
+    //velMax = 250;
+    //potencia = 500;
+    Moto() {
+        c.rodas = 2;
+        c.nome = "Moto";
+        //portas = 0;
+        //cor = "Preta";
+
+        //cout << velMax << endl;
+        //cout << potencia << endl;
+        cout << c.rodas << endl;
+        cout << c.nome << endl;
+        //cout << portas << endl;
+        //cout << cor << endl;}
+    }
+};
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+
+    Carro v1;
+    cout << endl << endl;
+    Moto v2;
+
+    return 0;
+}
